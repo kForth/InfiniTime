67efd31a0efaa6bcf61cc5bd5fd68de2903a5c01
Kestin Goforth <kgoforth1503@gmail.com>
2023-12-14 23:13:08 -0500

    Increment settings version

--- src/components/settings/Settings.h
+++ src/components/settings/Settings.h
@@ -286,7 +286,7 @@ namespace Pinetime {
     private:
       Pinetime::Controllers::FS& fs;
 
-      static constexpr uint32_t settingsVersion = 0x0006;
+      static constexpr uint32_t settingsVersion = 0x0007;
 
       struct SettingsData {
         uint32_t version = settingsVersion;
